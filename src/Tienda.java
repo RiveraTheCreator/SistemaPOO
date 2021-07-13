ada1dcc7b937b3a7666b95c8ec79fbe9c4d9c7c4
RiveraTheCreator <iriiverab1900@alumno.ipn.mx>
2021-07-13 18:40:27 -0500

    borrando la carpeta src

--- src/Tienda.java
+++
@@ -1,7 +0,0 @@
-
-public class Tienda {
-    public static void main(String [] args){
-        Departamento unDepto = new Departamento("Cosmeticos",1,10000.0);
-        unDepto.realizarmovimientos();
-    }
-}
